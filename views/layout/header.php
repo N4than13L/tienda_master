28ccd0a336f8c8142c679cc693020de33a764f7e
Jose Nathaniel Bonilla <natharevolution@gmail.com>
2026-05-21 11:45:23 -0400

    refactor: comment out header styles and clean up header structure

--- views/layout/header.php
+++ views/layout/header.php
@@ -13,34 +13,34 @@
 </head>
 
 <body>
-	<div class="m-2">
+	<div>
 		<!-- CABECERA + NAVBAR -->
-		<header id="header">
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				<div class="container-fluid">
-					<a class="navbar-brand d-flex align-items-center" href="<?= base_url ?>">
-						<img src="<?= base_url ?>assets/img/camiseta.png" alt="Camiseta Logo" width="40" height="40" class="d-inline-block align-text-top me-2" />
-						Tienda de camisetas
-					</a>
-					<!-- Sidebar toggle moved to sidebar.php -->
-					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-					<div class="collapse navbar-collapse" id="mainNavbar">
-						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-							<li class="nav-item">
-								<a class="nav-link" href="<?= base_url ?>">Inicio</a>
-							</li>
-							<?php $categorias = Utils::showCategorias(); ?>
-							<?php while ($cat = $categorias->fetch_object()): ?>
-								<li class="nav-item">
-									<a class="nav-link" href="<?= base_url ?>categoria/ver&id=<?= $cat->id ?>"><?= $cat->nombre ?></a>
-								</li>
-							<?php endwhile; ?>
-						</ul>
-					</div>
-				</div>
-			</nav>
-		</header>
 
-		<div id="content">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<div class="container-fluid">
+				<a class="navbar-brand d-flex align-items-center" href="<?= base_url ?>">
+					<img src="<?= base_url ?>assets/img/camiseta.png" alt="Camiseta Logo" width="40" height="40" class="d-inline-block align-text-top me-2" />
+					Tienda de camisetas
+				</a>
+				<!-- Sidebar toggle moved to sidebar.php -->
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="mainNavbar">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<li class="nav-item">
+							<a class="nav-link" href="<?= base_url ?>">Inicio</a>
+						</li>
+						<?php $categorias = Utils::showCategorias(); ?>
+						<?php while ($cat = $categorias->fetch_object()): ?>
+							<li class="nav-item">
+								<a class="nav-link" href="<?= base_url ?>categoria/ver&id=<?= $cat->id ?>"><?= $cat->nombre ?></a>
+							</li>
+						<?php endwhile; ?>
+					</ul>
+				</div>
+			</div>
+		</nav>
+
+
+		<div id="content" class="d-flex m-5 p-5">
